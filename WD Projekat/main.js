aa4805490629d9f6c111c556ee31b58f2109c669
Vaskoz123 <97392417+Vaskoz123@users.noreply.github.com>
2022-05-22 19:21:20 +0200

    Order v1

--- WD Projekat/main.js
+++ WD Projekat/main.js
@@ -15,3 +15,38 @@ hamburger.addEventListener("click",() =>{
     drugiBurger.classList.toggle("drugiBurger");
     treciBurger.classList.toggle("treciBurger");
 })
+let food=[];
+
+fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
+    .then(response=>{
+        if(!response.ok)
+            throw Error('Error');
+        return response.json();
+    })
+    .then((data)=>{
+        food=data;
+        renderFood(data);
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+const renderFood=(food)=>{
+    const foods = document.getElementById('foods');
+    
+    let resultFoodsHtml = '';
+
+    food.forEach(food => {
+        resultFoodsHtml += `
+        <div class="card text-white bg-warning mb-3" style="width: 18rem;">
+            <img class="card-img-top" src="${food.imageUrl}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${food.name}</h5>
+                <p class="card-text">${food.price}</p>
+                <button type="button" onclick="buyBook('${food.id}')"class="btn">Buy</button>
+            </div>
+        </div>
+        `;
+    });
+
+     foods.innerHTML = resultFoodsHtml;
+}
