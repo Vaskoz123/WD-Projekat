018dc0c73f98593048d221749ac506b44498f442
Vaskoz123 <97392417+Vaskoz123@users.noreply.github.com>
2022-05-22 22:18:37 +0200

    Edit Food Working

--- WD Projekat/main.js
+++ WD Projekat/main.js
@@ -18,7 +18,7 @@ hamburger.addEventListener("click",() =>{
 let food=[];
 
 fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
-    .then(response=>{
+    .then(response => {
         if(!response.ok)
             throw Error('Error');
         return response.json();
@@ -30,19 +30,20 @@ fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
     .catch(err=>{
         console.log(err);
     })
-const renderFood=(food)=>{
+const renderFood = (food) => {
     const foods = document.getElementById('foods');
     
     let resultFoodsHtml = '';
 
-    food.forEach(food => {
+    food.forEach ( food => {
         resultFoodsHtml += `
         <div class="card text-white bg-warning mb-3" style="width: 16rem;">
             <img class="card-img-top" src="${food.imageUrl}" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">${food.name}</h5>
-                <p class="card-text">${food.price}</p>
+                <p class="card-text">${food.price} KM</p>
                 <button type="button" onclick="buyFood('${food.id}')"class="btn">Buy</button>
+                <button type="button" onclick="editFood('${food.id}')"class="btn">Edit</button>
             </div>
         </div>
         `;
@@ -54,12 +55,12 @@ const buyFood = async (id) => {
     await fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food/${id}`, {
         method: "DELETE",
     }).then((response) => {
-        if(!response.ok){
-            alert('Error');
+        if(response.ok){
+            console.log(response);
         }
     })
 }
-const addFood= async() =>{
+const addFood = async () =>{
 const foodName = document.getElementById('foodName').value;
 const foodPrice = document.getElementById('foodPrice').value;
 const foodImage = document.getElementById('foodImage').value;
@@ -67,7 +68,9 @@ const foodID = document.getElementById('foodID').value;
 
 await fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`, {
     method: 'POST', 
-    headers: new Headers({'content-type': 'application/json'}),
+    headers: {
+        'content-type': 'application/json'
+    },
     body: JSON.stringify({
         "id": foodID,
         "name": foodName,
@@ -76,9 +79,45 @@ await fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`, {
         
     })
 })
-.then(res => {
-    if(!res.ok){
-        alert('Error');
+.then(response => {
+    if(response.ok){
+        console.log(response);
     }
 })
+}
+
+
+const editFood = () => {
+    document.getElementById('editFoodMenu').style.opacity='1';
+}
+
+
+
+
+const editFoodForm = async () => { 
+    const editedFoodID = document.getElementById('editfoodID').value;
+    const editedFoodImage = document.getElementById('editfoodImage').value;
+    const editedFoodName = document.getElementById('editfoodName').value;
+    const editedFoodPrice = document.getElementById('editfoodPrice').value;
+
+    await fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`, {
+        method: 'PUT', 
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id":  editedFoodID,
+            "name": editedFoodName,
+            "price": editedFoodPrice,
+            "imageUrl": editedFoodImage
+        })
+    })
+    .then(response => {
+        if(response.ok){
+            console.log(response);
+        }
+    })
+}
+const removeFoodForm = () => {
+    document.getElementById('editFoodMenu').style.opacity='0';
 }
